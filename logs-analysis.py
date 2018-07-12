d6410c8ce50f0b7a17805f9afbb2a31a6fe2b283
Abhinav Khare <abhinav.khare31@gmail.com>
2018-07-12 10:37:27 +0000

    Reduce query to one sql statement

--- logs-analysis.py
+++ logs-analysis.py
@@ -5,17 +5,11 @@ DB_NAME = "news"
 def top_3_articles(name):
     db = psycopg2.connect(database=name)
     c = db.cursor()
-    c.execute("select count(*), path from log where path like '/article/%' group by path order by count(*) desc limit 3;")
+    c.execute("select count(*), articles.title from log,articles where (log.path like '/article/%' AND log.status = '200 OK' AND substr(log.path,10) = articles.slug) group by articles.title order by count(*) desc limit 3;")
     articles = c.fetchall()
-    titles = []
-    views = []
     for article in articles:
-        slug = article[1].replace("/article/", "")
-        views.append(article[0])
-        c.execute("select title from articles where slug = '{}'".format(slug))
-        titles.append(c.fetchone()[0])
-    for title, view in zip(titles, views):
-        print('"{}" - {} views'.format(title, view))
+        print('"{}" - {} views'.format(article[1], article[0]))
     db.close()
+
 
 top_3_articles(DB_NAME)
