0231f48ca6c20fba26526ec35339a4d1ee8d8fd4
Abhinav Khare <abhinav.khare31@gmail.com>
2018-07-12 10:48:08 +0000

    Remove unnecessary argment from articles function

--- logs-analysis.py
+++ logs-analysis.py
@@ -2,8 +2,8 @@ import psycopg2
 
 DB_NAME = "news"
 
-def top_3_articles(name):
-    db = psycopg2.connect(database=name)
+def top_3_articles():
+    db = psycopg2.connect(database=DB_NAME)
     c = db.cursor()
     c.execute("select count(*), articles.title from log,articles where (log.path like '/article/%' AND log.status = '200 OK' AND substr(log.path,10) = articles.slug) group by articles.title order by count(*) desc limit 3;")
     articles = c.fetchall()
@@ -19,5 +19,5 @@ def top_3_authors():
     for author in authors:
         print('"{}" - {} views'.format(author[1], author[0]))
     db.close()
-top_3_articles(DB_NAME)
+top_3_articles()
 top_3_authors()
